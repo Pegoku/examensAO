813146426fde60212c2a073bb47789bce4da8167
Pegoku <pegoku08@gmail.com>
2024-12-17 17:42:41 +0100

    acabat

--- examen1.py
+++ examen1.py
@@ -3,10 +3,10 @@ import random
 def llegir_llista_enters():
     l = []
     while True:
-        a = int(input("introdueix un nombre"))
+        a = input("introdueix un nombre: ")
         if a == ".":
             break
-        l.append(a)
+        l.append(int(a))
     return l
 
 def senars_llista(llista):
@@ -14,37 +14,40 @@ def senars_llista(llista):
     # print(llista)
     if type(llista) == str:
         llista = llista.split(',')
+    # print(llista)
     for i in list(llista):
         if int(i) % 2 != 0:
-            l.append(i)
+            l.append(int(i))
     return l
 
 def sumar_parells_llista(llista):
     s = 0
-    print(llista)
-    llista = llista.split(',')
-        
+    if type(llista) == str:
+        llista = llista.split(',')    
     for i in llista:
-        if i % 2 == 0:
-            s = s + i
+        if int(i) % 2 == 0:
+            s = s + int(i)
     return s
 
 def cercar_numero_llista(llista, numero):
     # print(llista)
+    l = []
     if type(llista) == str:
         llista = llista.split(',')
-    
-    if numero not in llista:
+    for i in llista:
+        l.append(int(i))
+        
+    if numero not in l:
         return -1
     else:
-        for x, i in enumerate(llista):
+        for x, i in enumerate(l):
             if i == numero:
-                return x
+                return x+1
     
 def llegir_llista_paraules():
     l = []
     while True:
-        a = input("introdueix una paraula")
+        a = input("introdueix una paraula: ")
         if a == ".":
             break
         l.append(a)
@@ -53,7 +56,6 @@ def llegir_llista_paraules():
 def crear_paraula_llista(llista):
     l = ""
     
-    # print(llista)
     if type(llista) == str:
         llista = llista.split(',')
         
@@ -69,17 +71,15 @@ def crear_llista_num_aleatoris():
 
 def comparar_llistes(llista1, llista2):
     l = []
-    # print(llista)
     if type(llista1) == str:
         llista1 = llista1.split(',')
-    # print(llista)
     if type(llista2) == str:
         llista2 = llista2.split(',')
 
-    for a,b in llista1, llista2:
+    for a, b in zip(llista1, llista2):
         if a == b:
             l.append(2)
-        elif a in llista2:
+        elif b in llista1:
             l.append(1)
         else:
             l.append(0)
@@ -95,60 +95,64 @@ def majors_edat(edat1, edat2):
         print("Edat2 es major d'edat")
     else:
         print("Edat2 no es major d'edat")
-    
-# def ejecutar(func, nArgs):
-#     args = []
-#     for i in range(nArgs):
-#         args.append(input("Introdueix argument n.{}".format(i+1)))
-#     return func(*args)
         
 def menu():
-    print("Quina funcio vols ejecutar:")
-    print("1. Llegir llista d'enters")
-    print("2. Senars llista")
-    print("3. Sumar parells llista")
-    print("4. Cercar numero llista")
-    print("5. Llegir llista paraules")
-    print("6. Crear paraula de llista")
-    print("7. Crear llista de numeros aleatoris")
-    print("8. Comparar valors llistes")
-    print("9. Majors d'edat")
-    print("10. Sortir")
+    start = True
     while True:
-        a = int(input("Introdueix una opcio: "))
-        if a == 1:
+        if start:
+            print("Quina funcio vols ejecutar:")
+            print("1. Llegir llista d'enters")
+            print("2. Senars llista")
+            print("3. Sumar parells llista")
+            print("4. Cercar numero llista")
+            print("5. Llegir llista paraules")
+            print("6. Crear paraula de llista")
+            print("7. Crear llista de numeros aleatoris")
+            print("8. Comparar valors llistes")
+            print("9. Majors d'edat")
+            print("10. Sortir")
+            start = False
+        a = input("Introdueix una opcio: ")
+        if a == "1":
+            start = True
             print(llegir_llista_enters())
-        if a == 2:
+        if a == "2":
+            start = True
             x = input("Introdueix argument 1: ")
             print(senars_llista(x))
-        if a == 3:
+        if a == "3":
+            start = True
             x = input("Introdueix argument 1: ")
             print(sumar_parells_llista(x))
-        if a == 4:
+        if a == "4":
+            start = True
             x = input("Introdueix argument 1: ")
             y = int(input("Introdueix argument 2: "))
             print(cercar_numero_llista(x, y))
-        if a == 5:
+        if a == "5":
+            start = True
             print(llegir_llista_paraules())
-        if a == 6:
+        if a == "6":
+            start = True
             x = input("Introdueix argument 1: ")
             print(crear_paraula_llista(x))
-        if a == 7:
+        if a == "7":
+            start = True
             print(crear_llista_num_aleatoris())
-        if a == 8:
+        if a == "8":
+            start = True
             x = input("Introdueix argument 1: ")
             y = input("Introdueix argument 2: ")
             print(comparar_llistes(x, y))
-        if a == 9:
+        if a == "9":
+            start = True
             x = int(input("Introdueix argument 1: "))
             y = int(input("Introdueix argument 2: "))
             majors_edat(x, y)
-        if a == 10:
+        if a == "10":
+            start = True
             print("Adeu!!!")
             break
             
         
-                
-                
-# print(senars_llista())
 menu()
